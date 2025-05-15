bb70891ae681568a212bca3213f852b4a4ead854
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-15 19:56:47 -0300

    Update product price in SQL script

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,8 +1,3 @@
--- Seleciona todas as colunas.
 SELECT * FROM products
 
--- Seleciona colunas especificas.
-SELECT name, price FROM products
-
--- Definindo a ordem de exibição das colunas.
-SELECT price, name FROM products
+UPDATE products SET price = 45.90 WHERE id = 1
