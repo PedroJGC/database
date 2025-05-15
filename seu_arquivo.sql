39ec83aa866e758bdf0b97f95ad9c5f09951c0df
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-15 18:38:47 -0300

    SELECT

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,14 +1,8 @@
-/*
-CREATE TABLE products (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  price REAL NOT NULL,
-  category TEXT NULL DEFAULT 'general'
-)
-*/
-
--- Seleciona os registros
+-- Seleciona todas as colunas.
 SELECT * FROM products
 
-INSERT INTO products (name, price) VALUES ('Mouse', 50)
-INSERT INTO products (name, price, category) VALUES ('Teclado', 500.50, 'accessory')
+-- Seleciona colunas especificas.
+SELECT name, price FROM products
+
+-- Definindo a ordem de exibição das colunas.
+SELECT price, name FROM products
