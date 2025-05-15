8309989896961fa73aa836654c9267950dc8fba0
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-14 21:29:39 -0300

    Entering data and viewing records

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,1 +1,14 @@
-DROP TABLE products
+/*
+CREATE TABLE products (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  price REAL NOT NULL,
+  category TEXT NULL DEFAULT 'general'
+)
+*/
+
+-- Seleciona os registros
+SELECT * FROM products
+
+INSERT INTO products (name, price) VALUES ('Mouse', 50)
+INSERT INTO products (name, price, category) VALUES ('Teclado', 500.50, 'accessory')
