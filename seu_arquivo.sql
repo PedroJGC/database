04072f990f7d84d3fdd416a7f6143424361285b6
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-18 10:20:05 -0300

    Filtering Values

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,1 +1,16 @@
-SELECT * FROM products WHERE name LIKE '%head%' 
+SELECT * FROM products
+
+-- Seleciona valores diferentes.
+SELECT * FROM products WHERE price <> 800
+
+-- Seleciona valores maiores.
+SELECT * FROM products WHERE price > 500.50
+
+-- Seleciona valores menores.
+SELECT * FROM products WHERE price < 600
+
+-- Seleciona valor maior ou igual.
+SELECT * FROM products WHERE price >= 500.50
+
+-- Seleciona valor menor ou igual.
+SELECT * FROM products WHERE price <= 800
