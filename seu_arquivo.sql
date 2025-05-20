ce532aada2bb845c0152faee55a38642c5a23568
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-19 21:18:46 -0300

    Order of precedence

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,7 +1,1 @@
-SELECT * FROM products
-
--- O operador AND retorna os registros que atende todos os critérios (TRUE/TRUE).
-SELECT * FROM products WHERE price > 500 AND price < 1000
-
--- O operador OR retorna os registros que atende pelo menos um dos critérios (FALSE/TRUE).
-SELECT * FROM products WHERE price > 500 OR price < 1000
+SELECT * FROM products WHERE (price > 45 AND price < 1000) AND (category = 'audio' OR category = 'image') 
