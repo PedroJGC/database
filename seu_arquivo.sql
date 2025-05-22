0440fcb5102992cc494297e70bc0782c91b6ffd5
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-22 16:41:08 -0300

    LIMIT

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,12 +1,1 @@
--- ASC (ascentente/crescente)
-SELECT * FROM products ORDER BY price
-SELECT * FROM products ORDER BY price ASC
-
--- DESC (descendente/decrescente)
-SELECT * FROM products ORDER BY price DESC
-
--- WHERE + ORDER BY
-SELECT * FROM products WHERE category = 'audio' ORDER BY price DESC
-
--- ORDER BY com texto
-SELECT * FROM products ORDER BY name
+SELECT * FROM products ORDER BY price DESC LIMIT 3
