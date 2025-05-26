870c08e32dc64b3b51212b1df0f0e94dff18bf37
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-26 16:32:07 -0300

    populate students table with initial data

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,4 +1,12 @@
-CREATE TABLE students (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
-)
+INSERT INTO students (name) VALUES ('Lucas Santos');
+INSERT INTO students (name) VALUES ('Beatriz Lima');
+INSERT INTO students (name) VALUES ('Gabriel Oliveira');
+INSERT INTO students (name) VALUES ('Mariana Costa');
+INSERT INTO students (name) VALUES ('Felipe Souza');
+INSERT INTO students (name) VALUES ('Larissa Pereira');
+INSERT INTO students (name) VALUES ('Rafael Mendes');
+INSERT INTO students (name) VALUES ('Amanda Silva');
+INSERT INTO students (name) VALUES ('Juliana Rodrigues');
+INSERT INTO students (name) VALUES ('Pedro Ferreira');
+
+SELECT * FROM students
