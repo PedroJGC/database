df46f76baf626fd92d914895a38aa6e575c400fb
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-26 16:50:44 -0300

    populate courses table with initial data

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,4 +1,10 @@
-CREATE TABLE courses (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
-)
+INSERT INTO courses (name) VALUES ('HTML');
+INSERT INTO courses (name) VALUES ('CSS');
+INSERT INTO courses (name) VALUES ('Javascript');
+INSERT INTO courses (name) VALUES ('Typescript');
+INSERT INTO courses (name) VALUES ('React');
+INSERT INTO courses (name) VALUES ('Node.js');
+INSERT INTO courses (name) VALUES ('Git');
+INSERT INTO courses (name) VALUES ('Github');
+INSERT INTO courses (name) VALUES ('Express.js');
+INSERT INTO courses (name) VALUES ('Banco de dados');
