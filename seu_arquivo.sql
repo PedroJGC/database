1fca06fdc6ba299ea6f1a9e373e1272d5fb1464f
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-26 16:42:06 -0300

    create courses table and remove student insertions from SQL file

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,12 +1,4 @@
-INSERT INTO students (name) VALUES ('Lucas Santos');
-INSERT INTO students (name) VALUES ('Beatriz Lima');
-INSERT INTO students (name) VALUES ('Gabriel Oliveira');
-INSERT INTO students (name) VALUES ('Mariana Costa');
-INSERT INTO students (name) VALUES ('Felipe Souza');
-INSERT INTO students (name) VALUES ('Larissa Pereira');
-INSERT INTO students (name) VALUES ('Rafael Mendes');
-INSERT INTO students (name) VALUES ('Amanda Silva');
-INSERT INTO students (name) VALUES ('Juliana Rodrigues');
-INSERT INTO students (name) VALUES ('Pedro Ferreira');
-
-SELECT * FROM students
+CREATE TABLE courses (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+)
