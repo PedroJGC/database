99d59799eb39ae261f6f33ff0e6b0a17944ef641
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-05-27 17:46:24 -0300

    populate student_address table with initial data

--- seu_arquivo.sql
+++ seu_arquivo.sql
@@ -1,9 +1,9 @@
--- 1:1 (um para um)  um aluno possui um endereço, e um endereço pertence a um aluno.
-CREATE TABLE student_address (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-  student_id INTEGER UNIQUE NOT NULL,
-  street TEXT NOT NULL,
-  city TEXT NOT NULL,
-  
-  FOREIGN KEY (student_id) REFERENCES students(id)
-)
+INSERT INTO student_address
+(student_id, street, city)
+VALUES
+(1, 'Rua São jõao', 'São Paulo')
+
+INSERT INTO student_address
+(student_id, street, city)
+VALUES
+(2, 'Rua Dom Pedro', 'Rio de Janeiro')
